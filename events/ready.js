fd5fb21396f6c02a98b53a8ed5bf2bf4d642ccb2
RTGuinanao <54865442+RTGuinanao@users.noreply.github.com>
2021-09-17 02:30:20 -0700

    a

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,14 @@
 module.exports = async (client) => {
   client.Ready = true, 
-  client.user.setPresence({
-    status: "online",  // You can show online, idle, and dnd
-    activity: {
-        name: "international love",  // The message shown
-        type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
-    }
-});
+  setInterval(() => {
+        client.user.setPresence({
+        status: "online",  // You can show online, idle, and dnd
+        activity: {
+            name: "international love",  // The message shown
+            type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
+    });
+  }, 69000);
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
 client.RegisterSlashCommands();
