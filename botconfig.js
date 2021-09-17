52109d78014aa0847371b677c7f268a23f90c196
RTGuinanao <54865442+RTGuinanao@users.noreply.github.com>
2021-09-17 00:42:24 -0700

    AHHHH:

--- botconfig.js
+++ botconfig.js
@@ -1,35 +1,34 @@
 module.exports = {
-    Admins: ["UserID", "UserID"], //Admins of the bot
-    ExpressServer: true,//If you wanted to make the website run or not
-    DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
-    Port: 3000, //Which port website gonna be hosted
-    SupportServer: "https://discord.gg/6RxDVJme", //Support Server Link
-    Token: process.env.Token || "ODg4MjkwOTcyODM0ODg5NzYw.YUQjbw.YnOWzxMEMzn5D3SU3GRAmZVq1yo", //Discord Bot Token
-    ClientID: process.env.Discord_ClientID || "888290972834889760", //Discord Client ID
-    ClientSecret: process.env.Discord_ClientSecret || "35vGcjINbPuZ3NGWMq_nl6gqgY8nYtrX", //Discord Client Secret
-    Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-    CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
-    "24/7": false, //If you want the bot to be stay in the vc 24/7
-    CookieSecret: "Pikachu is EPIC", //A Secret like a password
-    IconURL:
-      "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
-    Permissions: 2205280576, //Bot Inviting Permissions
-    Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
-  
-    //Lavalink
-     Lavalink: {
-      id: "Main",
-      host: "lava.sudhan.tech",
-      port: 1234,
-      pass: "CodingWithSudhan!", 
-      secure: false, // Set this to true if you're self-hosting lavalink on replit.
-    },
-  
-  
-    //Please go to https://developer.spotify.com/dashboard/
-    Spotify: {
-      ClientID: process.env.Spotify_ClientID || "99f31ac613874820b8ad79ea0ee59d2c", //Spotify Client ID
-      ClientSecret: process.env.Spotify_ClientSecret || "b50ff6047474433dbfe1dd23ec6e2372", //Spotify Client Secret
-    },
-  };
-  
+  Admins: ["UserID", "UserID"], //Admins of the bot
+  ExpressServer: true,//If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
+  Token: process.env.Token || "", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "Pikachu is cute", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+
+  //Lavalink
+   Lavalink: {
+    id: "Main",
+    host: "lava.sudhan.tech",
+    port: 1234,
+    pass: "CodingWithSudhan", 
+    secure: false, // Set this to true if you're self-hosting lavalink on replit.
+  },
+
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+  },
+};
