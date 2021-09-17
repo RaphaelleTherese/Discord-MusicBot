f8324f1a1654256ef750ade31f7ec6e3f0abf21b
RTGuinanao <54865442+RTGuinanao@users.noreply.github.com>
2021-09-17 02:58:35 -0700

    please

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,12 @@
 module.exports = async (client) => {
   client.Ready = true, 
-  setInterval(() => {
-        let names = ["international love", "dat booty shake", "to mr worldwide", "my own concerts"];
-        let types = ["PLAYING", "WATCHING", "LISTENING", "STREAMING"];
-        let random = Math.floor(Math.random() * (names.length - 1) + 1);
-        client.user.setPresence({
-        status: "online",  // You can show online, idle, and dnd
-        activity: {
-            name: names[random],  // The message shown
-            type: types[random], // PLAYING, WATCHING, LISTENING, STREAMING,
-        }
-    });
-  }, 69000);
+  client.user.setPresence({
+    status: "online",  // You can show online, idle, and dnd
+    activity: {
+        name: "international love",  // The message shown
+        type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
+    }
+});
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
 client.RegisterSlashCommands();
